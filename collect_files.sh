5638afc2907aa37bbbd11a8c0707a1c6449f8007
vladimironni <vladimir.ptl.06@gmail.com>
2025-04-29 14:17:42 +0300

    test7

--- collect_files.sh
+++ collect_files.sh
@@ -17,30 +17,51 @@ copy_recursion() {
             local filename=$(basename "$file")
             local target="$output_dir/$filename"
             if [ -f "$target" ]; then
-                cp "$file" "${target}_+"
+                cp "$file" "${target}_copy"
                 if [ $? -eq 0 ]; then
-                    echo "Файл ${current_path:+$current_path/}$filename скопирован как ${filename}_+"  #если current_path не пуст и вообще существует, подставляем $current_path/. здесь использовал параметрическое расширение bash: "https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html"; Глава: 3.5.3 Shell Parameter Expansion
+                    echo "Файл ${current_path:+$current_path/}$filename скопирован как ${filename}_copy"  #если current_path не пуст и вообще существует, подставляем $current_path/. здесь использовал параметрическое расширение bash: "https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html"; Глава: 3.5.3 Shell Parameter Expansion
                 else
                     echo "Ошибка при копировании файла ${current_path:+$current_path/}$filename"
                 fi
+            else
+                cp "$file" "$target"
+                if [ $? -eq 0 ]; then
+                    echo "Файл ${current_path:+$current_path/}$filename успешно скопирован"
+                else
+                    echo "Ошибка при копировании файла ${current_path:+$current_path/}$filename"
+                fi
+            fi
+        fi
+    done
 
-
-
-    if [ $# -ne 2 ]; then
-        echo "Ошибка ввода. Укажите ровно два аргумента"
-        exit 1
-    fi
-
-    INPUT_DIR="$1"
-    OUTPUT_DIR="$2"
-
-    if [ ! -d "$INPUT_DIR" ]; then
-        echo "Входной директории '$INPUT_DIR' не существует"
-        exit 1
-    fi
-
-
-    echo "Копирование файлов из '$INPUT_DIR' в '$OUTPUT_DIR' успешно завершено"
+    for dir in "$input_dir"/*; do
+        if [ -d "$dir" ]; then
+            local dirname=$(basename "$dir")
+            local new_output_dir="$output_dir/$dirname"
+            copy_recursion "$dir" "$new_output_dir" "${current_path:+$current_path/}$dirname"
+        fi
+    done
 }
+
+
+if [ $# -ne 2 ]; then
+    echo "Ошибка ввода. Укажите ровно два аргумента"
+    exit 1
+fi
+
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+
+if [ ! -d "$INPUT_DIR" ]; then
+    echo "Входной директории '$INPUT_DIR' не существует"
+    exit 1
+fi
+
+echo "Произвожу копирование файлов из входной директории в выходную"
+
+copy_recursion "$INPUT_DIR" "$OUTPUT_DIR"
+
+echo "Копирование файлов из '$INPUT_DIR' в '$OUTPUT_DIR' успешно завершено"
+
             
 
