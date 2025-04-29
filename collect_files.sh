19aece31b3e61842a3e3ed35c297cecd8b14bc98
vladimironni <vladimir.ptl.06@gmail.com>
2025-04-29 14:09:58 +0300

    test3

--- collect_files.sh
+++ collect_files.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
 # Скрипт для копирования всех файлов из входной директории в выходную
 # Выполнял: Путилин Владимир Игоревич 
-# Дата: 
 
-
-if [ $# -ne 2 ]; then
-    echo "Ошибка ввода. Укажите ровно два аргумента"
-    exit 1
-fi
-
-INPUT_DIR="$1"
-OUTPUT_DIR="$2"
-
-if [ ! -d "$INPUT_DIR" ]; then
-    echo "Входной директории '$INPUT_DIR' не существует"
-    exit 1
-fi
-
-if [ ! -d "$OUTPUT_DIR" ]; then
-    echo "Выходной директории '$OUTPUT_DIR' не существует"
-    mkdir -p "$OUTPUT_DIR"
-fi
-
-echo "Произвожу копирование файлов из входной директории в выходную"
-for files in "$INPUT_DIR"/*; do
-    if [ -f "$files" ]; then
-        cp "$files" "$OUTPUT_DIR"/
-        echo "Файл $(basename "$files") успешно скопирован"
+copy_recursion() {
+    local input_dir="$1"
+    local output_dir="$2"
+    local current_path="$3" 
+    mkdir -p "$output_dir"
+    
+    if [ -n "$current_path" ]; then
+        echo "Директория $current_path создана в '$OUTPUT_DIR'"
     fi
-done
+    
+    for file in "$input_dir"/*; do
+        if [ -f "$file" ]; then
+            local filename=$(basename "$file")
+            local target="$output_dir/$filename"
+            if [ -f "$target" ]; then
+                cp "$file" "${target}_+"
+                if [ $? -eq 0 ]; then
+                    echo "Файл ${current_path:+$current_path/}$filename скопирован как ${filename}_+"  #если current_path не пуст и вообще существует, подставляем $current_path/. здесь использовал параметрическое расширение bash: "https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html"; Глава: 3.5.3 Shell Parameter Expansion
+                else
+                    echo "Ошибка при копировании файла ${current_path:+$current_path/}$filename"
+                fi
 
-for dr in "$INPUT_DIR"/*; do
-    if [ -d "$dr" ]; then
-        dir_name=$(basename "$dr")
-        mkdir -p "$OUTPUT_DIR/$dir_name"
-        echo "Директория $dir_name создана в "$OUTPUT_DIR""
-        for file in "$dr"/*; do
-            if [ -f "$file" ]; then
-                cp "$file" "$OUTPUT_DIR/$dir_name"/
-                echo "Файл $dir_name/$(basename "$file") успешно скопирован"
-            fi
-        done
+
+
+    if [ $# -ne 2 ]; then
+        echo "Ошибка ввода. Укажите ровно два аргумента"
+        exit 1
     fi
-done
 
-echo "Копирование файлов из '$INPUT_DIR' в '$OUTPUT_DIR' успешно завершено"
+    INPUT_DIR="$1"
+    OUTPUT_DIR="$2"
+
+    if [ ! -d "$INPUT_DIR" ]; then
+        echo "Входной директории '$INPUT_DIR' не существует"
+        exit 1
+    fi
+
+
+    echo "Копирование файлов из '$INPUT_DIR' в '$OUTPUT_DIR' успешно завершено"
+}
+            
 
